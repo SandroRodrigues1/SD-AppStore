f668275d0b7145a9f9d15cf6b9f4b6bc646d9c37
Sandro Rodrigues <nartenrodrigues1212@gmail.com>
2025-01-11 15:38:02 +0000

    metrics changes

--- src/grpc-proto/main.py
+++ src/grpc-proto/main.py
@@ -156,36 +156,6 @@ class ProductService(product_service_pb2_grpc.ProductServiceServicer):
             error_code="",
             timestamp="2024-12-28T23:00:00"
         )
-
-
-    def GetProductById(self, request, context):
-        start_time = time.time()
-        try:
-            connection = create_connection()
-            if connection is None:
-                ProductServiceMetrics.request_counter.labels(method="GetProductById", status="failure").inc()
-                return product_service_pb2.Product()
-
-            cursor = connection.cursor()
-            cursor.execute("SELECT id, name, price, description, image FROM products WHERE id = %s", (request.id,))
-            row = cursor.fetchone()
-
-            cursor.close()
-            connection.close()
-
-            if row:
-                ProductServiceMetrics.products_fetched.inc()  # Produto recuperado
-                ProductServiceMetrics.request_counter.labels(method="GetProductById", status="success").inc()  # Sucesso
-            else:
-                ProductServiceMetrics.request_counter.labels(method="GetProductById", status="failure").inc()  # Falha
-        except Exception as e:
-            ProductServiceMetrics.request_counter.labels(method="GetProductById", status="failure").inc()
-            ProductServiceMetrics.fetch_errors.inc()  # Incrementa erro de busca
-            raise e
-
-        duration = time.time() - start_time
-        ProductServiceMetrics.request_duration_histogram.labels(method="GetProductById").observe(duration)
-        return product_service_pb2.Product(id=row[0], name=row[1], price=row[2], description=row[3], image=row[4]) if row else product_service_pb2.Product()
     
     def DeleteProduct(self, request, context):
         try:
@@ -240,7 +210,8 @@ def serve():
         product_service_pb2_grpc.add_ProductServiceServicer_to_server(ProductService(), server)
         server.add_insecure_port('[::]:8080')
         print("Servidor iniciado na porta 8080...")
-        start_http_server(9090)  # Inicia o servidor Prometheus
+
+        start_http_server(9091)  # Inicia o servidor Prometheus
         server.start()
         server.wait_for_termination()
     except Exception as e:
